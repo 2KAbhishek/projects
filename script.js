f16c117b7720fd60817392066a4a6443e54224ed
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-30 12:23:22 +0530

    Add devicons support

--- script.js
+++ script.js
@@ -4,6 +4,36 @@ const repoList = document.querySelector('.repo-list');
 const reposSection = document.querySelector('.repos');
 const filterInput = document.querySelector('.filter-repos');
 
+let devicons = {
+    'C#': '<i class="devicon-csharp-plain colored"></i> C#',
+    'C++': '<i class="devicon-cplusplus-plain colored"></i> C++',
+    C: '<i class="devicon-c-plain colored"></i> C',
+    CSS: '<i class="devicon-css3-plain colored"></i> CSS',
+    Dockerfile: '<i class="devicon-docker-plain colored"></i> Docker',
+    Elixir: '<i class="devicon-elixir-plain colored"></i> Elixir',
+    Go: '<i class="devicon-go-plain colored"></i> Go',
+    HTML: '<i class="devicon-html5-plain colored"></i> HTML',
+    Haskell: '<i class="devicon-haskell-plain colored"></i> Haskell',
+    Java: '<i class="devicon-java-plain colored" style="color: #ffca2c"></i> Java',
+    JavaScript: '<i class="devicon-javascript-plain colored"></i> JavaScript',
+    'Jupyter Notebook': '<i class="devicon-jupyter-plain colored"></i> Jupyter',
+    Kotlin: '<i class="devicon-kotlin-plain colored" style="color: #796bdc"></i> Kotlin',
+    Lua: '<i class="devicon-lua-plain-wordmark colored" style="color: #0000d0"></i> Lua',
+    Nim: '<i class="devicon-nixos-plain colored" style="color: #FFC200"></i> Nim',
+    PHP: '<i class="devicon-php-plain colored"></i> PHP',
+    PLSQL: '<i class="devicon-sqlite-plain colored"></i> PLSQL',
+    Processing: '<i class="devicon-processing-plain colored" style="color: #0096D8"></i> Processing',
+    Python: '<i class="devicon-python-plain colored" style="color: #3472a6"></i> Python',
+    Ruby: '<i class="devicon-ruby-plain colored"></i> Ruby',
+    Rust: '<i class="devicon-rust-plain colored" style="color: #DEA584"></i> Rust',
+    Sass: '<i class="devicon-sass-original colored"></i> Sass',
+    Scala: '<i class="devicon-scala-plain colored"></i> Scala',
+    Shell: '<i class="devicon-bash-plain colored" style="color: #89E051"></i> Shell',
+    Swift: '<i class="devicon-swift-plain colored"></i> Swift',
+    TypeScript: '<i class="devicon-typescript-plain colored"></i> TypeScript',
+    null: '<i class="devicon-markdown-original"></i> Markdown',
+};
+
 // get information from github profile
 const getProfile = async function () {
     const res = await fetch(`https://api.github.com/users/${username}`, {
@@ -72,7 +102,7 @@ const displayRepos = function (repos) {
         listItem.innerHTML = `
             <h3>${repo.name}</h3>
             <span>${repo.description}</span> <br/><br/>
-            <span>${repo.language}</span> <br />
+            <span>${devicons[repo.language]}</span> <br />
             <br />
             <a href=${repo.homepage}>View Project</a>`;
         repoList.append(listItem);
