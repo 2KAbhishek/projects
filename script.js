9127065925835503943a9ae67b61c4a6e165ab04
chamodperera <chamodperera87@gmail.com>
2022-08-01 16:16:15 +0530

    added self

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const username = '2kabhishek';
+const username = 'chamodperera';
 const maxPages = 2;
 const repoList = document.querySelector('.repo-list');
 const reposSection = document.querySelector('.repos');
