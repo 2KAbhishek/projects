04a0538241f17d0bccb6b438f05543a359eaa0d8
WJ <82447031+Wjplatformer@users.noreply.github.com>
2022-08-11 17:54:40 +0800

    Update for PR

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const username = 'Wjplatformer';
+const username = '2KAbhishek';
 const maxPages = 2;
 const repoList = document.querySelector('.repo-list');
 const reposSection = document.querySelector('.repos');
